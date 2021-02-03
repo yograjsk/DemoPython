24da1fa8b719492f6cf5ecd558daa6a456895500
yograjsk <yograjsk@gmail.com>
2021-02-03 10:52:26 +0530

    sample selenium script

--- PythonBasics/Basic_assignment.py
+++ PythonBasics/Basic_assignment.py
@@ -5,5 +5,11 @@ list = [1,2,3,4,5,6,7,11,22,33,44,55,66,88]
 # listEven = []
 # listOdd = []
 
-
-
+list = [1,2,3,4,5,6,7,11,12,22,33,33,44,55,66,88,123,234,235,12,99,45,57]
+'''
+Out of above list, create below three more lists:
+divBy3 = list containing elements divisible by 3 alone
+divBy5 = list containing elements divisible by 5 alone
+divBy6 = list containing elements divisible by 6 alone
+Print the output of all three lists in the end
+'''
